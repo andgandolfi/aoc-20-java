9f6d48aa0389a9e1ac405388725c1a9dbecc0331
Andrea Gandolfi <gnd@google.com>
2021-12-03 16:33:55 +0100

    first commit

---
+++ src/xyz/gandolfi/aoc20/Utils.java
@@ -0,0 +1,23 @@
+package xyz.gandolfi.aoc20;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class Utils {
+    public static List<String> getInputFileLines(String resourcesInputFileName) {
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        try (InputStream is = classLoader.getResourceAsStream(resourcesInputFileName)) {
+            if (is == null) return null;
+            try (InputStreamReader isr = new InputStreamReader(is);
+                 BufferedReader reader = new BufferedReader(isr)) {
+                return reader.lines().toList();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
